3a4db477235a126f762b3add8bb17662ec4c9604
mueblegroup <mueblegroup@gmail.com>
2026-05-28 16:01:31 +0800

    Load notification routes with web middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,6 +3,7 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -10,7 +11,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function (): void {
-            require base_path('routes/notifications.php');
+            Route::middleware('web')->group(base_path('routes/notifications.php'));
         },
     )
     ->withMiddleware(function (Middleware $middleware): void {
