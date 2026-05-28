7f0e732883ba415dd2fcc66a17d9c7902e304e2c
mueblegroup <mueblegroup@gmail.com>
2026-05-28 15:48:11 +0800

    Fix notification route order

--- routes/notifications.php
+++ routes/notifications.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth'])->group(function () {
     Route::get('/notifications', [AppNotificationController::class, 'index'])->name('notifications.index');
+    Route::patch('/notifications/read-all', [AppNotificationController::class, 'markAllRead'])->name('notifications.read-all');
     Route::get('/notifications/{notification}', [AppNotificationController::class, 'show'])->name('notifications.show');
     Route::patch('/notifications/{notification}/read', [AppNotificationController::class, 'markRead'])->name('notifications.read');
-    Route::patch('/notifications/read-all', [AppNotificationController::class, 'markAllRead'])->name('notifications.read-all');
 });
 
 Route::middleware(['auth', 'role:admin'])
