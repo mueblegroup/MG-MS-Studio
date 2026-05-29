d78fd18c69a705fe93bf586e75fbd81cbd01a23f
mueblegroup <mueblegroup@gmail.com>
2026-05-29 18:32:40 +0800

    Register API routes and middleware

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Route;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
         commands: __DIR__ . '/../routes/console.php',
         health: '/up',
         then: function (): void {
@@ -18,6 +19,8 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => \App\Http\Middleware\CheckUserRole::class,
+            'api.ability' => \App\Http\Middleware\EnsureApiTokenCan::class,
+            'api.log' => \App\Http\Middleware\LogApiRequest::class,
         ]);
 
         $middleware->validateCsrfTokens(except: [
