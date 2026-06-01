a5fe9261f74664092da9ad5c53e4d5c24ea67cd3
mueblegroup <mueblegroup@gmail.com>
2026-06-02 01:40:58 +0800

    Add subscription renewal console command

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Services\HitPayService;
+use App\Services\SubscriptionClassService;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('subscriptions:bill-due-hitpay', function (SubscriptionClassService $subscriptions, HitPayService $hitpay) {
+    $count = $subscriptions->createDueHitpayRenewalOrders($hitpay);
+
+    $this->info("Processed {$count} due HitPay subscription renewal item(s).");
+})->purpose('Process due HitPay subscription renewals');
