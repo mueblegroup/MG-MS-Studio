7fd71a586d9009688ea483f9b5883791c44e214f
mueblegroup <mueblegroup@gmail.com>
2026-06-15 16:46:08 +0800

    Add superadmin studio and plan routes

--- routes/superadmin.php
+++ routes/superadmin.php
@@ -8,4 +8,12 @@ Route::middleware(['auth', 'role:superadmin'])
     ->name('superadmin.')
     ->group(function () {
         Route::get('/dashboard', [SuperadminController::class, 'dashboard'])->name('dashboard');
+
+        Route::get('/studios', [SuperadminController::class, 'studios'])->name('studios.index');
+        Route::get('/studios/{studio}/edit', [SuperadminController::class, 'editStudio'])->name('studios.edit');
+        Route::patch('/studios/{studio}', [SuperadminController::class, 'updateStudio'])->name('studios.update');
+
+        Route::get('/subscription-plans', [SuperadminController::class, 'plans'])->name('subscription-plans.index');
+        Route::post('/subscription-plans', [SuperadminController::class, 'storePlan'])->name('subscription-plans.store');
+        Route::patch('/subscription-plans/{plan}', [SuperadminController::class, 'updatePlan'])->name('subscription-plans.update');
     });
