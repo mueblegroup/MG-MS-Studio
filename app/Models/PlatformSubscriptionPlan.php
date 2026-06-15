178035a3ab362f0d8b0db5b982444a02cf99a025
mueblegroup <mueblegroup@gmail.com>
2026-06-15 16:44:22 +0800

    Add platform subscription plan model

---
+++ app/Models/PlatformSubscriptionPlan.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PlatformSubscriptionPlan extends Model
+{
+    protected $fillable = [
+        'name',
+        'slug',
+        'description',
+        'price',
+        'currency',
+        'billing_interval',
+        'max_students',
+        'max_teachers',
+        'max_admins',
+        'is_active',
+        'sort_order',
+    ];
+
+    protected $casts = [
+        'price' => 'decimal:2',
+        'is_active' => 'boolean',
+    ];
+
+    public function studios()
+    {
+        return $this->hasMany(Studio::class);
+    }
+
+    public function payments()
+    {
+        return $this->hasMany(PlatformSubscriptionPayment::class);
+    }
+}
