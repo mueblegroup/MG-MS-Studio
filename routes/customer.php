80e5c225f597c2ef45c304dd8e50030c7cb964d2
mueblegroup <mueblegroup@gmail.com>
2026-06-16 15:30:09 +0800

    Add professional client portal routes

--- routes/customer.php
+++ routes/customer.php
@@ -20,6 +20,10 @@ Route::middleware(['auth'])
     ->name('customer.')
     ->group(function (): void {
         Route::get('dashboard', [CustomerPortalController::class, 'dashboard'])->name('dashboard');
+        Route::get('studio', [CustomerPortalController::class, 'studio'])->name('studio');
+        Route::get('billing', [CustomerPortalController::class, 'billing'])->name('billing');
+        Route::get('invoices', [CustomerPortalController::class, 'invoices'])->name('invoices');
+
         Route::get('studios/create', [CustomerPortalController::class, 'createStudio'])->name('studios.create');
         Route::post('studios', [CustomerPortalController::class, 'storeStudio'])->name('studios.store');
         Route::get('studios/{studio}/open', [CustomerPortalController::class, 'launchStudio'])->name('studios.launch');
