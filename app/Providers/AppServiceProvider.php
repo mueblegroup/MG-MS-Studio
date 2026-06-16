cb9cebe2c72232641c225764aef4173e7349eafb
mueblegroup <mueblegroup@gmail.com>
2026-06-16 18:27:10 +0800

    Bind tenant manager as request singleton

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Support\TenantManager;
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
@@ -14,7 +15,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(TenantManager::class, function () {
+            return new TenantManager();
+        });
     }
 
     /**
