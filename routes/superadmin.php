00ea7ed0617dfa48d46922d48f5f4c2b6f995e7e
mueblegroup <mueblegroup@gmail.com>
2026-06-17 17:33:20 +0800

    Tighten superadmin central routing

--- routes/superadmin.php
+++ routes/superadmin.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Superadmin\SuperadminController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'role:superadmin'])
+Route::middleware(['auth', 'central', 'role:superadmin'])
     ->prefix('superadmin')
     ->name('superadmin.')
     ->group(function () {
@@ -14,6 +14,7 @@ Route::middleware(['auth', 'role:superadmin'])
         Route::patch('/studios/{studio}', [SuperadminController::class, 'updateStudio'])->name('studios.update');
 
         Route::get('/users', [SuperadminController::class, 'users'])->name('users.index');
+        Route::get('/domains', [SuperadminController::class, 'domains'])->name('domains.index');
 
         Route::get('/subscription-plans', [SuperadminController::class, 'plans'])->name('subscription-plans.index');
         Route::post('/subscription-plans', [SuperadminController::class, 'storePlan'])->name('subscription-plans.store');
