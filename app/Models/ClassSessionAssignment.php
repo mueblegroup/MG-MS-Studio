bf7f84e1a31655ad76c44e5aaf19467c23c41d24
mueblegroup <mueblegroup@gmail.com>
2026-07-12 17:39:37 +0800

    Scope class assignments to the current studio

--- app/Models/ClassSessionAssignment.php
+++ app/Models/ClassSessionAssignment.php
@@ -2,12 +2,13 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\AssignsStudio;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class ClassSessionAssignment extends Model
 {
-    use SoftDeletes;
+    use AssignsStudio, SoftDeletes;
 
     protected $fillable = [
         'studio_id',
