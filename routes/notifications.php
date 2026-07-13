b2f22da9a5bd2243980d4aa58d32deba0698cd55
mueblegroup <mueblegroup@gmail.com>
2026-07-13 14:06:05 +0800

    Remove duplicate admin notification routes

--- routes/notifications.php
+++ routes/notifications.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\Admin\AppNotificationController as AdminAppNotificationController;
 use App\Http\Controllers\AppNotificationController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,11 +9,3 @@ Route::middleware(['auth'])->group(function () {
     Route::get('/notifications/{notification}', [AppNotificationController::class, 'show'])->name('notifications.show');
     Route::patch('/notifications/{notification}/read', [AppNotificationController::class, 'markRead'])->name('notifications.read');
 });
-
-Route::middleware(['auth', 'role:admin'])
-    ->prefix('admin')
-    ->name('admin.')
-    ->group(function () {
-        Route::get('/notifications', [AdminAppNotificationController::class, 'index'])->name('notifications.index');
-        Route::post('/notifications', [AdminAppNotificationController::class, 'store'])->name('notifications.store');
-    });
