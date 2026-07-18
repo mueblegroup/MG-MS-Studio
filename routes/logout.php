264d965eeb489778d0f7548d6632ed7e9f22b82c
mueblegroup <mueblegroup@gmail.com>
2026-07-18 23:49:26 +0800

    Remove duplicate standalone logout route file

--- routes/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use App\Http\Controllers\LogoutController;
-use Illuminate\Support\Facades\Route;
-
-Route::match(['get', 'post'], '/logout', [LogoutController::class, 'logout'])
-    ->middleware('auth')
-    ->name('logout');
