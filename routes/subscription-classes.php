7f0972c56d0cb8c15b733b9c3f6d9534eebffbd8
mueblegroup <mueblegroup@gmail.com>
2026-07-21 15:49:39 +0800

    Allow assigned teachers to manage subscription students

--- routes/subscription-classes.php
+++ routes/subscription-classes.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\SubscriptionClassManagementController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     Route::get('/subscription-classes/{class}', [SubscriptionClassManagementController::class, 'show'])
         ->name('subscription-classes.show');
     Route::post('/subscription-classes/{class}/students/{subscription}/notify', [SubscriptionClassManagementController::class, 'notify'])
