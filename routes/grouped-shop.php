bd60e481041843235d5457441ec5bb3247c2bf13
mueblegroup <mueblegroup@gmail.com>
2026-07-21 16:27:05 +0800

    Delete duplicate grouped shop route file

--- routes/grouped-shop.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-use App\Http\Controllers\GroupedShopController;
-use Illuminate\Support\Facades\Route;
-
-Route::get('/shop', [GroupedShopController::class, 'index'])->name('shop.index');
-Route::get('/shop/', [GroupedShopController::class, 'index']);
