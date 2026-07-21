757cdda1c0e5c9d2d87be87f8c362b31dd116aa0
mueblegroup <mueblegroup@gmail.com>
2026-07-21 15:40:55 +0800

    Register subscription class management routes

---
+++ routes/subscription-classes.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\SubscriptionClassManagementController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
+    Route::get('/subscription-classes/{class}', [SubscriptionClassManagementController::class, 'show'])
+        ->name('subscription-classes.show');
+    Route::post('/subscription-classes/{class}/students/{subscription}/notify', [SubscriptionClassManagementController::class, 'notify'])
+        ->name('subscription-classes.students.notify');
+});
+
+Route::middleware(['auth', 'role:student'])->group(function () {
+    Route::post('/student/subscriptions/{subscription}/cancel', [SubscriptionClassManagementController::class, 'cancelStudentSubscription'])
+        ->name('student.subscriptions.cancel');
+});
