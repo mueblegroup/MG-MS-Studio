200448037b652288da4eee033f67708f22fe47bc
mueblegroup <mueblegroup@gmail.com>
2026-08-21 01:54:24 +0800

    Use Kuala Lumpur as platform timezone default

--- config/app.php
+++ config/app.php
@@ -34,8 +34,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | When your application is in debug mode, detailed error messages with
-    | stack traces will be shown on every error that occurs within your
-    | application. If disabled, a simple generic error page is shown.
+    | stack traces will be shown on every error. If disabled, a simple generic
+    | error page is shown.
     |
     */
 
@@ -47,8 +47,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | This URL is used by the console to properly generate URLs when using
-    | the Artisan command line tool. You should set this to the root of
-    | the application so that it's available within Artisan commands.
+    | Artisan commands.
     |
     */
 
@@ -59,23 +58,18 @@ return [
     | Application Timezone
     |--------------------------------------------------------------------------
     |
-    | Here you may specify the default timezone for your application, which
-    | will be used by the PHP date and date-time functions. The timezone
-    | is set to "UTC" by default as it is suitable for most use cases.
+    | The central/main-domain platform uses Asia/Kuala_Lumpur by default.
+    | Studio tenant requests may override this at runtime with the owner-selected
+    | studio timezone through ApplyStudioTimezone middleware.
     |
     */
 
-    'timezone' => 'UTC',
+    'timezone' => env('APP_TIMEZONE', 'Asia/Kuala_Lumpur'),
 
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
     |--------------------------------------------------------------------------
-    |
-    | The application locale determines the default locale that will be used
-    | by Laravel's translation / localization methods. This option can be
-    | set to any locale for which you plan to have translation strings.
-    |
     */
 
     'locale' => env('APP_LOCALE', 'en'),
@@ -88,11 +82,6 @@ return [
     |--------------------------------------------------------------------------
     | Encryption Key
     |--------------------------------------------------------------------------
-    |
-    | This key is utilized by Laravel's encryption services and should be set
-    | to a random, 32 character string to ensure that all encrypted values
-    | are secure. You should do this prior to deploying the application.
-    |
     */
 
     'cipher' => 'AES-256-CBC',
@@ -109,13 +98,6 @@ return [
     |--------------------------------------------------------------------------
     | Maintenance Mode Driver
     |--------------------------------------------------------------------------
-    |
-    | These configuration options determine the driver used to determine and
-    | manage Laravel's "maintenance mode" status. The "cache" driver will
-    | allow maintenance mode to be controlled across multiple machines.
-    |
-    | Supported drivers: "file", "cache"
-    |
     */
 
     'maintenance' => [
