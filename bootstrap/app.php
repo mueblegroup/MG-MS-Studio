88ee11b348f4f0220bc6e62a8ceaacfc4e21bb36
mueblegroup <mueblegroup@gmail.com>
2026-08-21 01:27:11 +0800

    Apply studio timezone before tenant controllers

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->web(prepend: [
             \App\Http\Middleware\ResolveStudioTenant::class,
+            \App\Http\Middleware\ApplyStudioTimezone::class,
             \App\Http\Middleware\ApplyStudioPaymentGatewayConfig::class,
         ]);
 
