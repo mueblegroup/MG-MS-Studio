c1a3dc0c166351ba3881ea5d5ce34884fd10cb47
mueblegroup <mueblegroup@gmail.com>
2026-09-14 15:33:12 +0800

    Scope attendance records to studios

--- app/Models/Attendance.php
+++ app/Models/Attendance.php
@@ -2,11 +2,15 @@
 
 namespace App\Models;
 
+use App\Models\Concerns\AssignsStudio;
 use Illuminate\Database\Eloquent\Model;
 
 class Attendance extends Model
 {
+    use AssignsStudio;
+
     protected $fillable = [
+        'studio_id',
         'booking_id',
         'class_session_assignment_id',
         'plan_session_id',
