70330960a6cb5583d50f8694ec29e38477b490cb
mueblegroup <mueblegroup@gmail.com>
2026-09-14 10:10:59 +0800

    Load teacher calendar events dynamically

--- resources/views/teacher/dashboard.blade.php
+++ resources/views/teacher/dashboard.blade.php
@@ -163,7 +163,7 @@
                     initialView: 'dayGridMonth',
                     headerToolbar: { left: 'prev', center: 'title', right: 'next' },
                     height: 'auto',
-                    events: @json($calendarEvents ?? [])
+                    events: @json(route('calendar.events'))
                 });
                 calendar.render();
             });
