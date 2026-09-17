d54097fd4c296975dc1d01220c39832f0177140c
mueblegroup <mueblegroup@gmail.com>
2026-09-17 23:59:12 +0800

    Use canonical ClassM8 logo component

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,8 +1,1 @@
-<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 300 300" role="img" aria-label="ClassM8 logo" {{ $attributes }}>
-    <g fill="#3b5650">
-        <circle cx="105" cy="78" r="42" />
-        <circle cx="195" cy="78" r="42" />
-        <circle cx="105" cy="170" r="42" />
-        <circle cx="195" cy="170" r="42" />
-    </g>
-</svg>
+<img src="{{ asset('images/mueble-logo.svg') }}" alt="ClassM8" {{ $attributes->merge(['class' => 'object-contain bg-white']) }}>
