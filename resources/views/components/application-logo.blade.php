4b544e6e70a4c1e55a02a1e4647f65ee38a6da3d
mueblegroup <mueblegroup@gmail.com>
2026-09-18 00:08:51 +0800

    Use canonical ClassM8 PNG logo asset

--- resources/views/components/application-logo.blade.php
+++ resources/views/components/application-logo.blade.php
@@ -1,1 +1,1 @@
-<img src="{{ asset('images/mueble-logo.svg') }}" alt="ClassM8" {{ $attributes->merge(['class' => 'object-contain bg-white']) }}>
+<img src="{{ asset('images/branding/classm8-logo.png') }}" alt="ClassM8" {{ $attributes->merge(['class' => 'object-contain bg-white']) }}>
